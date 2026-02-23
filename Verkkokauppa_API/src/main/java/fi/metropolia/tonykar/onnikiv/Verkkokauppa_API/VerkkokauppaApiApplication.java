e739eb975765fb947045ab8a02c5ae02a1a42e79
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2026-02-23 13:44:04 +0200

    Initial commit

---
+++ Verkkokauppa_API/src/main/java/fi/metropolia/tonykar/onnikiv/Verkkokauppa_API/VerkkokauppaApiApplication.java
@@ -0,0 +1,13 @@
+package fi.metropolia.tonykar.onnikiv.Verkkokauppa_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerkkokauppaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerkkokauppaApiApplication.class, args);
+	}
+
+}
---
+++ Verkkokauppa_API/src/test/java/fi/metropolia/tonykar/onnikiv/Verkkokauppa_API/VerkkokauppaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.metropolia.tonykar.onnikiv.Verkkokauppa_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerkkokauppaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
