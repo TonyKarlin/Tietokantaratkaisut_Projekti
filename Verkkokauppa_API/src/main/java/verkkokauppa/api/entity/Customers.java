80d694c263884843884e009284bffeed1e411a38
TonyKarlin <tony.karlin@metropolia.fi>
2026-02-23 20:49:58 +0200

    feat: added a customer entity and a repository

---
+++ Verkkokauppa_API/src/main/java/verkkokauppa/api/entity/Customers.java
@@ -0,0 +1,73 @@
+package verkkokauppa.api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="customers")
+public class Customers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public Customers() {
+    }
+
+    public Customers(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ Verkkokauppa_API/src/main/java/verkkokauppa/api/repository/CustomersRepository.java
@@ -0,0 +1,11 @@
+package verkkokauppa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import verkkokauppa.api.entity.Customers;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+}
