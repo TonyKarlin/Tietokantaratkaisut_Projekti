4d920de4009a4b68b14a5715dd103db4b800f9f8
onnik <onni.kivinen@gmail.com>
2026-02-23 16:04:58 +0200

    Update environment configuration and replace dotenv dependency

--- Verkkokauppa_API/src/main/java/verkkokauppa/api/Main.java
+++ Verkkokauppa_API/src/main/java/verkkokauppa/api/Main.java
@@ -3,11 +3,23 @@ package verkkokauppa.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+
+        // Ladataan tietokantainfot .env tiedostosta erikseen, koska springboot v4 + pom dotenv-java dependencyn kautta
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./Verkkokauppa_API/") // location of .env directory
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry
+                -> System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(Main.class, args);
     }
-
 }
