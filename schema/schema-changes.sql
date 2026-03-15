743cdbf53547aede6746279889b332d66f410b18
TonyKarlin <tony.karlin@metropolia.fi>
2026-03-15 16:27:43 +0200

    chore: add unique constraints to customeraddresses and supplieraddresses tables

--- schema/schema-changes.sql
+++ schema/schema-changes.sql
@@ -14,17 +14,21 @@ CREATE TABLE customeraddresses
 (
     id          INT PRIMARY KEY,
     customer_id INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES addresses (id),
-    FOREIGN KEY (customer_id) REFERENCES customers (id)
+    CONSTRAINT fk_customeraddresses_address FOREIGN KEY (id) REFERENCES addresses (id),
+    CONSTRAINT fk_customeraddresses_customer FOREIGN KEY (customer_id) REFERENCES customers (id),
+    CONSTRAINT uq_customeraddresses_customer UNIQUE (customer_id)
 );
+
 
 CREATE TABLE supplieraddresses
 (
     id          INT PRIMARY KEY,
     supplier_id INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES addresses (id),
-    FOREIGN KEY (supplier_id) REFERENCES suppliers (id)
+    CONSTRAINT fk_supplieraddresses_address FOREIGN KEY (id) REFERENCES addresses (id),
+    CONSTRAINT fk_supplieraddresses_supplier FOREIGN KEY (supplier_id) REFERENCES suppliers (id),
+    CONSTRAINT uq_supplieraddresses_supplier UNIQUE (supplier_id)
 );
+
 
 # Adding discounted_price column to orderitems table
 # This column will store the price of the item after applying any discounts.
